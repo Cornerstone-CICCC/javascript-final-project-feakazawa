a484e22fcf94ca156b72da19e8f6611918d0365a
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-06-11 14:13:51 -0700

    ex 12 done

--- src/ex12.js
+++ src/ex12.js
@@ -5,10 +5,41 @@
  * @returns {{characters: number, words: number, sentences: number}}
  */
 function analyzeText(str) {
+  let sentences = 0;
+
+  if (str === undefined) throw new Error("String cannot be undefined");
+
+  if (typeof str !== "string") throw new Error("Argument must be a string");
+
+  const trimWord = str.trim();
+
+  //only spaces
+  if (str.length > 0 && trimWord.length === 0) {
+    sentences = 0;
+  }
+
+  const splitWord = trimWord.split(" ").filter((word) => word !== "");
+
+  const hasPoint = splitWord.find((word) => word[word.length - 1] === ".");
+  const hasExclamation = splitWord.find(
+    (word) => word[word.length - 1] === "!",
+  );
+  const hasQuestion = splitWord.find((word) => word[word.length - 1] === "?");
+
+  if (splitWord.length > 0 && !hasPoint && !hasExclamation && !hasQuestion) {
+    sentences = 1;
+  } else {
+    sentences =
+      str.split(".").length -
+      1 +
+      (str.split("!").length - 1) +
+      (str.split("?").length - 1);
+  }
+
   return {
     characters: str.length,
-    words: str.split(' ').length,
-    sentences: str.split('.').length - 1,
+    words: splitWord.length,
+    sentences,
   };
 }
 

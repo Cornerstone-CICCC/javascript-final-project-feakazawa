9b1da3a669a458635bb82f586a88cc98ea069439
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-06-11 14:37:22 -0700

    ex 13 done

--- src/ex13.js
+++ src/ex13.js
@@ -5,7 +5,22 @@
  * @returns {boolean}
  */
 function checkAnagram(firstString, secondString) {
-  return firstString === secondString;
+  if (firstString === undefined)
+    throw new Error("First string cannot be undefined");
+
+  if (secondString === undefined)
+    throw new Error("Second string cannot be undefined");
+
+  if (typeof firstString !== "string" || typeof secondString !== "string")
+    throw new Error("Arguments must be strings");
+
+  const firstArr = firstString.split("");
+  const secArr = secondString.split("");
+
+  const firstSort = firstArr.sort().join("");
+  const secSort = secArr.sort().join("");
+
+  return firstSort.localeCompare(secSort) === 0 ? true : false;
 }
 
 module.exports = checkAnagram;

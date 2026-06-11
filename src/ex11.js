5f8ed23aa7618c8869b2c9f753071f8922147252
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-06-11 12:04:53 -0700

    ex 11 done

--- src/ex11.js
+++ src/ex11.js
@@ -6,7 +6,20 @@
  * @returns {Array}
  */
 function rotateArray(arr, positions) {
-  const splitIndex = arr.length - positions;
+  if (!arr) throw new Error("Array cannot be undefined");
+
+  if (positions === undefined) throw new Error("Positions cannot be undefined");
+
+  if (typeof positions !== "number")
+    throw new Error("Positions must be a number");
+
+  if (!Number.isInteger(positions))
+    throw new Error("Positions must be an integer");
+
+  if (!Array.isArray(arr)) throw new Error("Argument must be an array");
+
+  const splitIndex =
+    positions > 0 ? arr.length - positions : -1 * positions - arr.length;
   return [...arr.slice(splitIndex), ...arr.slice(0, splitIndex)];
 }
 

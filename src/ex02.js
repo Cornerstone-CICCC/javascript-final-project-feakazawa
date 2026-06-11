82361ed08b22c3416bf6c977d5936489aceb88f1
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-06-11 09:40:59 -0700

    ex 2 done

--- src/ex02.js
+++ src/ex02.js
@@ -4,6 +4,15 @@
  * @returns {number}
  */
 function sumArray(arr) {
+  if (!arr) throw new Error("Array cannot be undefined");
+
+  if (!Array.isArray(arr)) throw new Error("Argument must be an array");
+
+  if (arr.length <= 0) return 0;
+
+  if (arr.find((num) => typeof num !== "number"))
+    throw new Error("Array can only contain numbers");
+
   return arr.reduce((total, number) => total + number);
 }
 

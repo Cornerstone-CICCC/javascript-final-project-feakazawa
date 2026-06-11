8d4e7475479b32f9ba6b38b0cfad93d3bc453cc4
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-06-11 09:36:21 -0700

    ex 1 done

--- src/ex01.js
+++ src/ex01.js
@@ -4,9 +4,19 @@
  * @returns {number}
  */
 function findMax(arr) {
-  if (arr.length <= 0) throw new Error('Array cannot be empty');
+  if (!arr) throw new Error("Array cannot be undefined");
 
-  return arr[0];
+  if (!Array.isArray(arr)) throw new Error("Argument must be an array");
+
+  if (arr.length <= 0) throw new Error("Array cannot be empty");
+
+  const notNumber = arr.find((value) => typeof value !== "number");
+  if (notNumber === null || notNumber > 0)
+    throw new Error("Array can only contain numbers");
+
+  const sortArr = arr.toSorted((a, b) => b - a);
+
+  return sortArr[0];
 }
 
 module.exports = findMax;

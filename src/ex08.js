127e3c555fc970eba6261de3e193c7094e21a8fe
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-06-11 10:47:09 -0700

    ex 8 done

--- src/ex08.js
+++ src/ex08.js
@@ -6,7 +6,21 @@
  * @returns {string[]}
  */
 function search(arr, keyword) {
-  return arr.filter((item) => item.includes(keyword));
+  if (!arr) throw new Error("Array cannot be undefined");
+
+  if (keyword === undefined) throw new Error("Keyword cannot be undefined");
+
+  if (typeof keyword !== "string") throw new Error("Keyword must be a string");
+
+  if (!Array.isArray(arr)) throw new Error("Argument must be an array");
+
+  const notString = arr.find((item) => typeof item !== "string");
+  if (notString > 0 || notString === null)
+    throw new Error("Array can only contain strings");
+
+  return arr.filter((item) =>
+    item.toLowerCase().includes(keyword.toLowerCase()),
+  );
 }
 
 module.exports = search;

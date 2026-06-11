0701fded99b567b0f2676ee49a5209238242fb60
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-06-11 15:06:18 -0700

    ex 14 done

--- src/ex14.js
+++ src/ex14.js
@@ -5,7 +5,27 @@
  * @returns {number[]}
  */
 function moveZeroes(numbers) {
-  return numbers.sort();
+  const moveNums = [];
+  let count = 0;
+
+  if (!numbers) throw new Error("Array cannot be undefined");
+
+  if (!Array.isArray(numbers)) throw new Error("Argument must be an array");
+
+  const notNumber = numbers.find((num) => typeof num !== "number");
+  if (notNumber || notNumber === null)
+    throw new Error("Array can only contain numbers");
+
+  numbers.map((num, idx) => {
+    num !== 0 ? moveNums.push(num) : count++;
+  });
+
+  for (let i = 0; i < count; i++) {
+    moveNums.push(0);
+  }
+
+  return moveNums;
 }
+moveZeroes([0, 1, 0, 3, 12]);
 
 module.exports = moveZeroes;

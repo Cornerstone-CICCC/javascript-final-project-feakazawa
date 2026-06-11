aa81008621ff7efcc63fc9275532037c344c35ed
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-06-11 10:30:17 -0700

    ex 7 done

--- src/ex07.js
+++ src/ex07.js
@@ -4,6 +4,14 @@
  * @returns {{positive: number[], negative: number[], zero: number[]}}
  */
 function classifyNumbers(arr) {
+  if (!arr) throw new Error("Array cannot be undefined");
+
+  if (!Array.isArray(arr)) throw new Error("Argument must be an array");
+
+  const notNumber = arr.find((value) => typeof value !== "number");
+  if (notNumber === null || notNumber > 0)
+    throw new Error("Array can only contain numbers");
+
   return {
     positive: arr.filter((number) => number > 0),
     negative: arr.filter((number) => number < 0),
